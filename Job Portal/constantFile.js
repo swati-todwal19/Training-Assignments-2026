6e7dc03651271b901f4935d0d86447a1f42133c1
swati-todwal19 <swati.todwal@intimetec.com>
2026-01-26 12:03:54 +0530

    Modified constant file with session storage

--- Job Portal/constantFile.js
+++ Job Portal/constantFile.js
@@ -9,8 +9,14 @@ NO_HYPERLINKS: true
 }
 };
 
-
 export const LOCAL_STORAGE_KEYS = {
 APPLIED_JOBS: 'appliedJobs',
-SAVED_JOBS: 'savedJobs'
+SAVED_JOBS: 'savedJobs',
+RESUME_UPLOAD: "resumeUpload"
+};
+
+export const SESSION_STORAGE_KEYS = {
+  FILTERS: "sessionFilters",   
+  SEARCH: "sessionSearch",     
+  SORT: "sessionSort"          
 };
