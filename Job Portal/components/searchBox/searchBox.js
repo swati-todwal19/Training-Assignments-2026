e2a634f523d5e86edadc05be5112901b66921aef
swati-todwal19 <swati.todwal@intimetec.com>
2026-01-26 12:07:58 +0530

    Modified searchBox.js file with session storage

--- Job Portal/components/searchBox/searchBox.js
+++ Job Portal/components/searchBox/searchBox.js
@@ -1,26 +1,41 @@
+import { SESSION_STORAGE_KEYS } from "../../constantFile.js";
+
 export function initSearchBox(allJobs, renderJobsCallback, renderDefaultJobs) {
   const searchInput = document.querySelector("#globalSearchInput");
   const searchBtn = document.getElementById("search-btn");
 
-  searchBtn.addEventListener("click", () => {
-  const searchText = searchInput.value.trim().toLowerCase();
-  
-  if (!searchText) {
-    renderDefaultJobs(allJobs);
-    return;
+  const savedSearch = sessionStorage.getItem(SESSION_STORAGE_KEYS.SEARCH) || "";
+  if (savedSearch) {
+    searchInput.value = savedSearch;
+
+    applySearch(savedSearch);
   }
 
-  const filteredJobs = allJobs.filter(job => {
-  const { title, location, type } = job;   
+  function applySearch(searchText) {
+    const trimmedText = searchText.trim().toLowerCase();
 
-  return (
-    title.toLowerCase().includes(searchText) ||
-    location.toLowerCase().includes(searchText) ||
-    type.toLowerCase().includes(searchText)
-  );
-});
+    if (!trimmedText) {
+      renderDefaultJobs(allJobs);
+      return;
+    }
 
-  renderJobsCallback(filteredJobs);
+    const filteredJobs = allJobs.filter(job => {
+      const { title, location, type } = job;
+      return (
+        title.toLowerCase().includes(trimmedText) ||
+        location.toLowerCase().includes(trimmedText) ||
+        type.toLowerCase().includes(trimmedText)
+      );
+    });
 
+    renderJobsCallback(filteredJobs);
+  }
+
+  searchBtn.addEventListener("click", () => {
+    const searchText = searchInput.value;
+    
+    sessionStorage.setItem(SESSION_STORAGE_KEYS.SEARCH, searchText);
+
+    applySearch(searchText);
   });
 }
