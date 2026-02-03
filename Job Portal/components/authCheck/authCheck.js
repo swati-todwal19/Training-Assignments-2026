ec5a03a0fbd04cef43efa5b11d0f36b0a134cafe
swati-todwal19 <swati.todwal@intimetec.com>
2026-02-03 06:28:08 +0530

    Access control unverified users view mode

---
+++ Job Portal/components/authCheck/authCheck.js
@@ -0,0 +1,41 @@
+import { STORAGE_KEYS } from "../../constant.js";
+export function isUserLoggedIn() {
+    const sessionUser = sessionStorage.getItem(STORAGE_KEYS.TOKEN);
+    return !!sessionUser;
+}
+export function showGuestPopup(actionType) {
+    if (document.querySelector(".guest-popup"))
+        return;
+    const popup = document.createElement("div");
+    popup.className = "guest-popup";
+    let message = "";
+    let btnText = "";
+    let redirectPath = "";
+    if (actionType === "bookmark" || actionType === "apply") {
+        message = `Please register or login first to ${actionType} a job.`;
+        btnText = "Register / Login";
+        redirectPath = "components/authorization/register/register.html";
+    }
+    popup.innerHTML = `
+    <div class="popup-content">
+      <p>${message}</p>
+      <button id="authActionBtn">${btnText}</button>
+      <button id="closePopupBtn">Close</button>
+    </div>
+  `;
+    document.body.appendChild(popup);
+    const closeBtn = popup.querySelector("#closePopupBtn");
+    closeBtn.addEventListener("click", () => popup.remove());
+    const authBtn = popup.querySelector("#authActionBtn");
+    authBtn.addEventListener("click", () => {
+        window.location.href = redirectPath;
+    });
+}
+export function requireAuth(actionType, callback) {
+    if (isUserLoggedIn()) {
+        callback();
+    }
+    else {
+        showGuestPopup(actionType);
+    }
+}
